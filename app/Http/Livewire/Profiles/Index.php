142d3e6b25cad5fa4e7fca856c3dee6be372fc14
SebrinaAmira <69838503+SebrinaAmira@users.noreply.github.com>
2021-03-04 16:22:24 +0700

    profile

--- app/Http/Livewire/Profiles/Index.php
+++ app/Http/Livewire/Profiles/Index.php
@@ -22,7 +22,12 @@ class Index extends Component
 
     public function render()
     {
-        $profiless = Profil::all();
+        $profiless = Profil::orderBy('id', 'ASC')->first();
+        if($profiless) {
+            $this->edit($profiless->id);
+        } else {
+            $this->openForm();
+        }
 
         return view('livewire.profiles.index', ['profiless'=>$profiless])
             ->extends('layouts.master');
@@ -33,7 +38,7 @@ class Index extends Component
         $this->validateOnly($propertyName);
     }
 
-    public function edit($id)
+    public function store()
     {
         $profiless = $this->validate([
             'alamat' => 'required',
@@ -44,7 +49,31 @@ class Index extends Component
             'status' => 'required',
         ]);
 
-        // Profil::updated(['id' => $this->profile_id], [
+        if($this->profile_id) {
+            Profil::where('id', $this->profile_id)->update([
+                'alamat' => $this->alamat,
+                'fb' => $this->fb,
+                'instagram' => $this->instagram,
+                'telepon' => $this->telepon,
+                'deskripsi_konten' => $this->deskripsi_konten,
+                'status' => $this->status,
+                'created_by' => Auth::user()->id,
+                'updated_by' => Auth::user()->id,
+            ]);
+        } else {
+            Profil::create([
+                'alamat' => $this->alamat,
+                'fb' => $this->fb,
+                'instagram' => $this->instagram,
+                'telepon' => $this->telepon,
+                'deskripsi_konten' => $this->deskripsi_konten,
+                'status' => $this->status,
+                'created_by' => Auth::user()->id,
+                'updated_by' => Auth::user()->id,
+            ]);
+        }
+
+        // Profil::updateOrCreate(['id' => $this->profile_id], [
         //     'alamat' => $this->alamat,
         //     'fb' => $this->fb,
         //     'instagram' => $this->instagram,
@@ -55,6 +84,13 @@ class Index extends Component
         //     'updated_by' => Auth::user()->id,
         // ]);
 
+        $this->reset();
+        $this->closeForm();
+        
+    }
+
+    public function edit($id)
+    {
         // return $id;
         $profiless = Profil::find($id);
         $this->profile_id = $id;
@@ -65,33 +101,18 @@ class Index extends Component
         $this->deskripsi_konten = $profiless->deskripsi_konten;
         $this->status = $profiless->status;
 
-        
+        $this->openForm();
     }
 
-    // public function edit($id)
-    // {
-    //     // return $id;
-    //     $profiless = Profil::find($id);
-    //     $this->profile_id = $id;
-    //     $this->alamat = $profiless->alamat;
-    //     $this->fb = $profiless->fb;
-    //     $this->instagram = $profiless->instagram;
-    //     $this->telepon = $profiless->telepon;
-    //     $this->deskripsi_konten = $profiless->deskripsi_konten;
-    //     $this->status = $profiless->status;
+    public function delete($id)
+    {
+        $profiless = Profil::where('id',$id)->first();
+        $this->profile_id = $id;
 
-    //     $this->openForm();
-    // }
-
-    // public function delete($id)
-    // {
-    //     $profiless = Profil::where('id',$id)->first();
-    //     $this->profile_id = $id;
-
-    //     if($id){
-    //         Profil::where('id',$id)->delete();
-    //     }
-    // }
+        if($id){
+            Profil::where('id',$id)->delete();
+        }
+    }
 
     public function openForm()
     {
--- resources/views/livewire/profiles/index.blade.php
+++ resources/views/livewire/profiles/index.blade.php
@@ -13,7 +13,7 @@
         @if ($isForm == false)  
 
         <div class="card-body">
-            <div class="col-md-3">
+            {{-- <div class="col-md-3">
                 <button wire:click="create()" class="btn btn-primary">Tambah Data</button>
             </div>
             <br>
@@ -55,7 +55,47 @@
                     </tr>
                     @endforeach
                 </tbody>
-            </table>
+            </table> --}}
+            <form wire:submit.prevent="store">
+                <input type="hidden" name="profile_id" wire:model="profile_id">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Facebook</label>
+                            <input type="text" class="form-control @error('fb') is-invalid @enderror" wire:model="fb" id="exampleFormControlInput1" value="{{ old('fb')}}">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Instagram</label>
+                            <input type="text" class="form-control @error('instagram') is-invalid @enderror" wire:model="instagram" id="exampleFormControlInput1" value="{{ old('instagram')}}">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Telepon</label>
+                            <input type="text" class="form-control @error('telepon') is-invalid @enderror" wire:model="telepon" id="exampleFormControlInput1" value="{{ old('telepon')}}">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Status</label>
+                            <select class="form-control @error('status') is-invalid @enderror" wire:model="status" id="exampleFormControlSelect1">
+                                <option value="">Pilih</option>
+                                <option value="Show" {{ old('status') == 'Show' ? 'selected' : ''}}>Show</option>
+                                <option value="Hide" {{ old('status') == 'Pria' ? 'selected' : ''}}>Hide</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Alamat</label>
+                    <textarea class="form-control @error('alamat') is-invalid @enderror" wire:model="alamat" id="exampleFormControlTextarea1" rows="3">{{ old('alamat')}}</textarea>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Deskripsi Konten</label>
+                    <textarea class="form-control @error('deskripsi_konten') is-invalid @enderror" wire:model="deskripsi_konten" id="exampleFormControlTextarea1" rows="3">{{ old('deskripsi_konten')}}</textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-info">Simpan</button>
+                </div>
+            </form>
         </div>
 
         @endif
